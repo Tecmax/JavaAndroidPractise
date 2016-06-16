3b1ead11dc39906661a4a1123fe68ccbe0440b1c
rakeshraki123 <poosala.rakesh@gmail.com>
2016-06-16 08:52:40 +0530

    Rakes new project

---
+++ JavaTestMahesh/src/com/mahesh/stringHandelling/Split.java
@@ -0,0 +1,9 @@
+package com.mahesh.stringHandelling;
+
+public class Split {
+	public static void main(String[] args) {
+		String m=new String(" i am madhesh");
+	
+//		m.getChars(5, 10, null, 0);
+	}
+	}

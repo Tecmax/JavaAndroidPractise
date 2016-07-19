edc6dd048f305e3a126135a3624b6df4e821fc26
sreevishwa <sreevishwa@gmail.com>
2016-07-19 08:19:49 +0530

    Java core Basics -vishwa

---
+++ VishwaCoreJava/src/com/vishwa/corejava/HelloWorld.java
@@ -0,0 +1,7 @@
+package com.vishwa.corejava;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello");
+	}
+}
---
+++ VishwaCoreJava/src/com/vishwa/corejava/Methods.java
@@ -0,0 +1,8 @@
+package com.vishwa.corejava;
+
+public class Methods {
+	public static void main(String[] args) {
+
+	}
+}
+ 	

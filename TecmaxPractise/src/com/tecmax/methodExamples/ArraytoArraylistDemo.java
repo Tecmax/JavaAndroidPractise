488d5cee0509b4ffce691d9f13907f686cc65f27
swetha <swetha@tecmax.com>
2018-06-21 21:38:58 +0530

    converting array to arraylist

---
+++ TecmaxPractise/src/com/tecmax/methodExamples/ArraytoArraylistDemo.java
@@ -0,0 +1,19 @@
+package com.tecmax.methodExamples;
+
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+
+public class ArraytoArraylistDemo {
+
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+             String [] sa= {"swetha","madhuri","nandini"};
+             List<String> abList= Arrays.asList(sa);
+             System.out.println(abList);
+	}
+
+}
--- TecmaxPractise/src/com/tecmax/methodExamples/arraylist.java
+++ TecmaxPractise/src/com/tecmax/methodExamples/arraylist.java
@@ -1,5 +1,26 @@
 package com.tecmax.methodExamples;
 
+import java.util.ArrayList;
+
 public class arraylist {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<String> a=new ArrayList<String>();
+		a.add("swetha");
+		a.add("theja");
+		a.add("joshna");
+		a.add("priyanka");
+		System.out.println(a);
+		a.add(0,"nandini");
+		System.out.println(a);
+		int pos=a.indexOf("madhuri");
+		String str=a.get(12);
+		int numberofitems=a.size();
+		System.out.println("current arraylist position stringvalue size is:"+"\n"+ numberofitems);
+		a.remove("dileep");
+		System.out.println(a);
+		
+	}
+
 
 }

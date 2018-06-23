9493858b9800620375055fd57fefd4ddb1086de6
nandini <nandini@tecmax.com>
2018-06-22 19:30:53 -0700

    arrays to arraylist

--- TecmaxPractise/src/com/tecmax/methodExamples/arrays.java
+++ TecmaxPractise/src/com/tecmax/methodExamples/arrays.java
@@ -1,5 +1,29 @@
 package com.tecmax.methodExamples;
 
+import java.util.ArrayList;
+
 public class arrays {
+	public class array {
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+	             ArrayList<String> a=new ArrayList<String>();
+	             a.add("nan");
+	             a.add("madhuri");
+	             a.add("swetha");
+	             a.add("dileep");
+	             System.out.println(a);
+	             a.remove("swetha");
+	             System.out.println(a);
+	             int pos=a.indexOf("Theja");
+	             String str=a.get(12);
+	             int numberofitems=a.size();
+	             System.out.println("current array List posiion Stringvalue size is:"+"\n"+a+"\n"+numberofitems);
+	             
+		}
+
+	}
+
+	public static String[] asList;
 
 }
---
+++ TecmaxPractise/src/com/tecmax/methodExamples/arraytoarraylist.java
@@ -0,0 +1,17 @@
+package com.tecmax.methodExamples;
+
+import java.awt.List;
+import java.util.Arrays;
+
+public class arraytoarraylist {
+
+	public static void main(String[] args) {
+
+		// TODO Auto-generated method stub
+		String[] a = { "dillu", "dillo", "sweha" };
+		List<String> aa=Arrays.asList(a);
+		System.out.println(aa);
+
+	}
+
+}
